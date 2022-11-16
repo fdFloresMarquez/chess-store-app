781d49422b2a383664f22aad0ce97013bffc36ca
Facundo <floresfacundo68@gmail.com>
2022-11-16 19:07:46 -0300

    update: Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,16 @@
-import './Home.css';
+import styles from './Home.module.css';
+
+import HeadingGroup from '@/components/HeadingGroup';
 
 const Home: React.FC = () => {
+  const headingsAmount: number = 7;
+
   return (
     <>
       <section>
-        <div className="container-fluid px-0 content">
+        <div className={`container-fluid px-0 ${styles.content}`}>
           <div className="row align-items-center">
-            <div className="col-6 text-center d-none d-lg-block" id="headingGroup">
-              <h1>
-                CHESS <span>/</span> AND <span>/</span> KNOWLEDGE
-              </h1>
-              <h1>
-                CHESS <span>/</span> AND <span>/</span> KNOWLEDGE
-              </h1>
-              <h1>
-                CHESS <span>/</span> AND <span>/</span> KNOWLEDGE
-              </h1>
-              <h1>
-                CHESS <span>/</span> AND <span>/</span> KNOWLEDGE
-              </h1>
-              <h1>
-                CHESS <span>/</span> AND <span>/</span> KNOWLEDGE
-              </h1>
-              <h1>
-                CHESS <span>/</span> AND <span>/</span> KNOWLEDGE
-              </h1>
-              <h1>
-                CHESS <span>/</span> AND <span>/</span> KNOWLEDGE
-              </h1>
-            </div>
+            <HeadingGroup commitedFieldsToAdd={headingsAmount} />
             <div className="col-lg-6">
               <img className="img-fluid" src="assets/board.jpg" />
             </div>
@@ -37,17 +19,19 @@ const Home: React.FC = () => {
       </section>
 
       <section>
-        <div className="container-fluid px-0 content">
+        <div className={`container-fluid px-0 ${styles.content}`}>
           <div className="row align-items-center">
             <div className="col-md-6 text-center order-md-2">
               <div className="row justify-content-center">
-                <div className="col-10 col-lg-8 blurb mb-5 mb-md-0">
-                  <h2>Chess Store</h2>
+                <div className={`col-10 col-lg-8 mb-5 mb-md-0 ${styles.blurb}`}>
+                  <h2 className={styles.h2}>Openings the right way!</h2>
                   <img className="d-none d-lg-inline" src="assets/crown.png" />
-                  <p className="lead">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, necessitatibus
-                    enim ratione nisi minus, alias hic eius delectus sit praesentium fuga iure
-                    provident, consectetur doloribus maiores. Minus iste fugiat molestias.
+                  <p className={`lead ${styles.p}`}>
+                    Have you ever wondered why 1.e4 and 1.d4 are the most popular openings? Would
+                    you like to know more about openings? Then this is the site for you! By learning
+                    book moves and the idea of chess theory you will start having more success in
+                    the opening. Improve your opening knowledge and get great positions from the
+                    start of the game!
                   </p>
                 </div>
               </div>
@@ -58,17 +42,18 @@ const Home: React.FC = () => {
           </div>
         </div>
 
-        <div className="container-fluid px-0 content">
+        <div className={`container-fluid px-0 ${styles.content}`}>
           <div className="row align-items-center">
             <div className="col-md-6 text-center">
               <div className="row justify-content-center">
-                <div className="col-10 col-lg-8 blurb mb-5 mb-md-0">
-                  <h2>Chess Store</h2>
+                <div className={`col-10 col-lg-8 mb-5 mb-md-0 ${styles.blurb}`}>
+                  <h2 className={styles.h2}>Play the ancient game</h2>
                   <img className="d-none d-lg-inline" src="assets/crown.png" />
-                  <p className="lead">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis, quo? Id
-                    delectus a nesciunt quibusdam explicabo fugit quia perspiciatis, fugiat, nobis
-                    expedita molestias ipsum fuga. Repellendus minima officia adipisci dolorum.
+                  <p className={`lead ${styles.p}`}>
+                    Have you ever had a queen or a rook versus your opponent&apos;s lone king but
+                    couldn&apos;t figure out how to win? Have you ever stalemated an opponent&apos;s
+                    lone king? Then this course for you! By learning these different types of
+                    checkmates you will start winning more of your games!
                   </p>
                 </div>
               </div>
@@ -79,17 +64,17 @@ const Home: React.FC = () => {
           </div>
         </div>
 
-        <div className="container-fluid px-0 content">
+        <div className={`container-fluid px-0 ${styles.content}`}>
           <div className="row align-items-center">
             <div className="col-md-6 text-center order-md-2">
               <div className="row justify-content-center">
-                <div className="col-10 col-lg-8 blurb mb-5 mb-md-0">
-                  <h2>Chess Store</h2>
+                <div className={`col-10 col-lg-8 mb-5 mb-md-0 ${styles.blurb}`}>
+                  <h2 className={styles.h2}>Become a Master</h2>
                   <img className="d-none d-lg-inline" src="assets/crown.png" />
-                  <p className="lead">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, ab! Nemo
-                    voluptas illo enim! Officiis corporis, amet est illum optio accusamus eligendi
-                    sunt iste vel, incidunt doloribus, aliquid pariatur odit.
+                  <p className={`lead ${styles.p}`}>
+                    From A1 to H8, become familiar with the whole board. Learn the middlegames and
+                    endgames like never before. Win tournaments and be the player you always wanted
+                    to be.
                   </p>
                 </div>
               </div>
